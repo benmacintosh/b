db6949230f8a60362ae72bb8f35a88a28d4e77ca
benmacintosh <ben.macintosh0@gmail.com>
2021-07-17 17:02:32 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,38 +11,19 @@ class App extends Component {
         <div>ben macintosh selected</div>
         <p/>
 
-        <a href="https://benmacintosh.github.io/orderForm/" style={{color:'blue'}}>fleurs order form</a><div></div>        
-        <a href="https://drive.google.com/open?id=1Y2klXRjGOIJTqKySc2zoEqAn3l9WmKx9" style={{color:'blue'}}>https://drive.google.com/open?id=1Y2klXRjGOIJTqKySc2zoEqAn3l9WmKx9</a><div></div>
+        <a href="https://benmacintosh.github.io/freereview/6.html" style={{color:'blue'}}>freereview</a><div></div>
         <a href="https://benmacintosh.github.io/meetingPointOfShortestTravel/" style={{color:'blue'}}>https://benmacintosh.github.io/meetingPointOfShortestTravel/</a><div></div>
-        <a href="https://benmacintosh.github.io/sfcnn/" style={{color:'blue'}}>https://benmacintosh.github.io/sfcnn/</a>
+        <a href="https://benmacintosh.github.io/walkForTime/" style={{color:'blue'}}>https://benmacintosh.github.io/walkForTime/</a><div></div>
 
         <div>new precedence</div>
         <div>new</div>
         <a href="https://google.com" style={{color:'blue'}}>google</a>
         <div></div>
         <a href="https://www.youtube.com/watch?v=MxwvtaswDYY" style={{color:'blue'}}>https://www.youtube.com/watch?v=MxwvtaswDYY</a> <div></div>
-        <a href="https://drive.google.com/open?id=1TkU_3pCP1_GgNQi1a_JKRGwaxE-HG3uZ" style={{color:'blue'}}>https://drive.google.com/open?id=1TkU_3pCP1_GgNQi1a_JKRGwaxE-HG3uZ </a> <div></div>
         <a href="https://drive.google.com/open?id=1UMUVF1wjC0I4xV33OJNL4nfvhX3BCBVF" style={{color:'blue'}}>New folder</a>
         <p/>
-
-        {/*<div>tools</div>
-
-        // <div>writing</div
-        */}
-
-        <a href="https://docs.google.com/document/d/1BNvit8TB-QKYj4tXKSBwHQVzKCHXGiV5mCnj7NIpYpc/edit?usp=sharing" style={{color:'blue'}}>Ben macintosh selected dennis rodman and ernst herbeck 30/10/18</a><div></div>
-        <a href="https://docs.google.com/document/d/1rzdybIq5gyWLpJsGp173Za9xnz8_cxemQVyK3HxO3E0/edit?usp=sharing" style={{color:'blue'}}>https://docs.google.com/document/d/1rzdybIq5gyWLpJsGp173Za9xnz8_cxemQVyK3HxO3E0/edit?usp=sharing</a><div></div>
-        <div>gardening/floristry</div>   
-        <a href="https://drive.google.com/open?id=1DjjcdPYN1j7c2EsNROreW2KpkhduQtXD" syle={{color:'blue'}}>https://drive.google.com/open?id=1DjjcdPYN1j7c2EsNROreW2KpkhduQtXD</a>
-        <p/>
-
-        <div>cv</div>        
-        <a href="https://drive.google.com/open?id=1JobQ1qgPOFjpesU8tSAxJcxyaRh-dm8r" syle={{color:'blue'}}>https://drive.google.com/open?id=1JobQ1qgPOFjpesU8tSAxJcxyaRh-dm8r </a>
-        <p/>
-
-        <a href="https://www.instagram.com/0008000000macintosh/" style={{color:'blue'}}>https://www.instagram.com/0008000000macintosh/</a>
+        
         <div></div>
-        <img src={img2} alt="" width="88"/>
         <img src={img} alt="" width="88"/>
 
 
