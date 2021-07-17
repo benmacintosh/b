5d3896aa502a3fc2d7f4ef6c26295716442fb87c
Ben Macintosh <ben.macintosh0@gmail.com>
2021-07-17 17:52:07 +1000

    aac

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component} from 'react';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
-import img from './img1.jpg'
+
+import aac from './aac.js'
+import img from './img.png'
 import img2 from './img2.png'
 
 class App extends Component {
@@ -10,8 +13,11 @@ class App extends Component {
 
         <div>ben macintosh selected</div>
         <p/>
-
         <a href="https://benmacintosh.github.io/freereview/6.html" style={{color:'blue'}}>freereview</a><div></div>
+
+        <Route path="/aac" component={aac} />
+        <a href='/aac'>AAC</a> <div></div>
+
         <a href="https://benmacintosh.github.io/meetingPointOfShortestTravel/" style={{color:'blue'}}>https://benmacintosh.github.io/meetingPointOfShortestTravel/</a><div></div>
         <a href="https://benmacintosh.github.io/walkForTime/" style={{color:'blue'}}>https://benmacintosh.github.io/walkForTime/</a><div></div>
 
@@ -19,12 +25,11 @@ class App extends Component {
         <div>new</div>
         <a href="https://google.com" style={{color:'blue'}}>google</a>
         <div></div>
-        <a href="https://www.youtube.com/watch?v=MxwvtaswDYY" style={{color:'blue'}}>https://www.youtube.com/watch?v=MxwvtaswDYY</a> <div></div>
         <a href="https://drive.google.com/open?id=1UMUVF1wjC0I4xV33OJNL4nfvhX3BCBVF" style={{color:'blue'}}>New folder</a>
         <p/>
         
         <div></div>
-        <img src={177023767_763016217678599_1231804918011744028_n} alt="" width="88"/>
+        <img src={img} alt="" width="88"/>
 
 
 
---
+++ src/aac.js
@@ -0,0 +1,24 @@
+
+
+import React, { Component} from 'react';
+import './App.css';
+
+class aac extends Component {
+  render() {
+    return (
+
+<div>
+
+        <div>ALGORITHMS AGAINST COMPUTATION</div>
+
+
+
+</div>
+
+    );
+  }
+}
+
+export default aac;
+
+
